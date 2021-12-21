1a967429f42a5e8eddb561cde0cab5669e63867f
smhrd <smhrd@59.0.124.72>
2021-12-21 10:41:27 +0900

    우경 - 안드로이드/DeviceDAO deviceLocation()메소드 생성

--- Fipl/src/model/DeviceDAO.java
+++ Fipl/src/model/DeviceDAO.java
@@ -79,7 +79,7 @@ public class DeviceDAO {
 	// 전체 장치목록
 	public ArrayList<DeviceVO> allList(String device_dept) {
 		// 세션에서 가져온 사용자의 부서를 입력받아서 해당 부서의 기기만 보여주게 한다
-				
+
 		al = new ArrayList<DeviceVO>();
 
 		try {
@@ -87,7 +87,7 @@ public class DeviceDAO {
 
 			String sql = "select * from DEVICES where DEVICE_DEPT=?";
 			psmt = conn.prepareStatement(sql);
-			
+
 			psmt.setString(1, device_dept);
 
 			rs = psmt.executeQuery();
@@ -116,42 +116,42 @@ public class DeviceDAO {
 	}
 
 	// 이상기기 장치목록
-		public ArrayList<DeviceVO> errorDevice(String device_dept) {
-			al = new ArrayList<DeviceVO>();
+	public ArrayList<DeviceVO> errorDevice(String device_dept) {
+		al = new ArrayList<DeviceVO>();
 
-			try {
-				Connection();
+		try {
+			Connection();
 
-				String sql = "select * from DEVICES where DEVICE_STATUS='1' and DEVICE_DEPT=?";
-				psmt = conn.prepareStatement(sql);
+			String sql = "select * from DEVICES where DEVICE_STATUS='1' and DEVICE_DEPT=?";
+			psmt = conn.prepareStatement(sql);
 
-				psmt.setString(1, device_dept);
+			psmt.setString(1, device_dept);
 
-				rs = psmt.executeQuery();
+			rs = psmt.executeQuery();
 
-				while (rs.next()) {
-					String get_device_no = rs.getString(1);
-					String get_device_date = rs.getString(2);
-					String get_device_loc = rs.getString(3);
-					String get_device_status = rs.getString(4);
-					String get_device_dept = rs.getString(5);
+			while (rs.next()) {
+				String get_device_no = rs.getString(1);
+				String get_device_date = rs.getString(2);
+				String get_device_loc = rs.getString(3);
+				String get_device_status = rs.getString(4);
+				String get_device_dept = rs.getString(5);
 
-					vo = new DeviceVO(get_device_no, get_device_date, get_device_loc, get_device_status, get_device_dept);
+				vo = new DeviceVO(get_device_no, get_device_date, get_device_loc, get_device_status, get_device_dept);
 
-					al.add(vo);
+				al.add(vo);
 
-				}
-
-			} catch (Exception e) {
-				System.out.println("장치목록 DAO 실패");
-				e.printStackTrace();
-
-			} finally {
-				close();
 			}
-			return al;
+
+		} catch (Exception e) {
+			System.out.println("장치목록 DAO 실패");
+			e.printStackTrace();
+
+		} finally {
+			close();
 		}
-	
+		return al;
+	}
+
 	// 장치 상태 업데이트
 	public int statusUpdate(String device_no) {
 		// 기기 이상 데이터가 들어온 기기 번호
@@ -174,6 +174,36 @@ public class DeviceDAO {
 			close();
 		}
 		return cnt;
+	}
+
+	// 디바이스 위치 검색
+	public String deviceLocation(String device_no) {
+		
+		String get_device_loc="";
+
+		try {
+			Connection();
+
+			String sql = "select * from DEVICES where DEVICE_no=?";
+			psmt = conn.prepareStatement(sql);
+
+			psmt.setString(1, device_no);
+
+			rs = psmt.executeQuery();
+
+			if (rs.next()) {
+				get_device_loc = rs.getString("device_no");
+			}
+
+		} catch (Exception e) {
+			System.out.println("장치목록 DAO 실패");
+			e.printStackTrace();
+
+		} finally {
+			close();
+		}
+		
+		return get_device_loc;
 	}
 
 }
